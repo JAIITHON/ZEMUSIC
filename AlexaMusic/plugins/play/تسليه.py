1448fbfa5e8032332c5a0a164a5aa18ff292ab40
RRR8RR <139396961+RRR8RR@users.noreply.github.com>
2023-12-30 05:55:35 +0300

    Update تسليه.py

--- AlexaMusic/plugins/play/تسليه.py
+++ AlexaMusic/plugins/play/تسليه.py
@@ -84,7 +84,7 @@ REPLY_MESSAGE_BUTTONS = [
     ]
 ]
 
-@app.on_message(filters.regex("^/AFYN"))
+@app.on_message(filters.regex("الاوامر"))
 async def cpanel(_, message: Message):             
         text = REPLY_MESSAGE
         reply_markup = ReplyKeyboardMarkup(REPLY_MESSAGE_BUTTONS, resize_keyboard=True, selective=True)
