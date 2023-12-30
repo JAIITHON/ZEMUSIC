bd0181297f4e8cdd1b336f2b3b63255007454209
RRR8RR <139396961+RRR8RR@users.noreply.github.com>
2023-12-30 05:07:54 +0300

    Update تجربه.py

--- AlexaMusic/plugins/play/تجربه.py
+++ AlexaMusic/plugins/play/تجربه.py
@@ -3,24 +3,24 @@ from AlexaMusic import app
 import asyncio
 from pytgcalls import PyTgCalls, StreamType
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from AlexaMusic import app
+from AlexaMusic.core.call import Alexa
 from AlexaMusic.utils.database import *
 from pytgcalls.exceptions import (NoActiveGroupCall,TelegramServerError,AlreadyJoinedError)
 
-@app.on_message(filters.regex("^مين في الكول$"))
+@app.on_message(filters.regex("^من في المكالمه$"))
 async def strcall(client, message):
-    assistant = await group_assistant(Yukki,message.chat.id)
+    assistant = await group_assistant(Alexa,message.chat.id)
     try:
-        await assistant.join_group_call(message.chat.id, AudioPiped("./assets/vega.mp3"), stream_type=StreamType().pulse_stream)
-        text="🙃🔔 الارانب  المتواجدين في الكول :\n\n"
+        await assistant.join_group_call(message.chat.id, AudioPiped("./Alexa/assets/call.mp3"), stream_type=StreamType().pulse_stream)
+        text="الناس الكاعده تسولف :\n\n"
         participants = await assistant.get_participants(message.chat.id)
         k =0
         for participant in participants:
             info = participant
             if info.muted == False:
-                mut="يتحدث 🗣"
+                mut="يحجي"
             else:
-                mut="ساكت 🔕"
+                mut="ساكت"
             user = await client.get_users(participant.user_id)
             k +=1
             text +=f"{k}➤{user.mention}➤{mut}\n"
@@ -29,21 +29,21 @@ async def strcall(client, message):
         await asyncio.sleep(7)
         await assistant.leave_group_call(message.chat.id)
     except NoActiveGroupCall:
-        await message.reply(f"ارنب الكول مش مفتوح اصلااا\n🤔")
+        await message.reply(f"المكالمه مغلقه الان")
     except TelegramServerError:
-        await message.reply(f"ارسل الامر تاني في مشكله في سيرفر التلجرام\n🤔")
+        await message.reply(f"قم بارسال الامر مره اخرى")
     except AlreadyJoinedError:
-        text="🙃🔔 الارانب  المتواجدين في الكول:\n\n"
+        text="الناس الكاعده تسولف :\n\n"
         participants = await assistant.get_participants(message.chat.id)
         k =0
         for participant in participants:
             info = participant
             if info.muted == False:
-                mut="يتحدث 🗣"
+                mut="يحجي"
             else:
-                mut="ساكت 🔕"
+                mut="ساكت"
             user = await client.get_users(participant.user_id)
             k +=1
             text +=f"{k}➤{user.mention}➤{mut}\n"
-        text += f"\nعددهم : {len(participants)}\n✔️"    
+        text += f"\العدد : {len(participants)}\n✔️"    
         await message.reply(f"{text}")
