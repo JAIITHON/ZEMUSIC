2adcb176c205565d267f4ad2cc0085a9ebee2af6
RRR8RR <139396961+RRR8RR@users.noreply.github.com>
2023-12-30 07:50:55 +0300

    Update انذار.py

--- AlexaMusic/plugins/play/انذار.py
+++ AlexaMusic/plugins/play/انذار.py
@@ -1,5 +1,16 @@
-from pyrogram import Client, filters
+import asyncio
+import os
+import time
+import requests
+import aiohttp
+from pyrogram import filters
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from strings.filters import command
+from AlexaMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 from AlexaMusic import app
+from asyncio import gather
+from pyrogram.errors import FloodWait
 
 ahmed = {}
 tom_max = 3
